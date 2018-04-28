236e936dffc8881b7fd0db0910492c3a813ea7b7
annprog <tecbbs@qq.com>
2018-04-28 14:25:05 +0800

    [demo-container] fix getenv bug

--- demo-container/init.sh
+++ demo-container/init.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 chown -R nobody.nobody /home/wwwroot/default/
-env | sed "s/\(.*\)=\(.*\)/env[\1]='\2'/" > /etc/php7/php-fpm.d/env.conf
+env |grep -v "=$" | sed "s/\(.*\)=\(.*\)/env[\1]='\2'/" >> /etc/php7/php-fpm.d/www.conf
 supervisord -n
