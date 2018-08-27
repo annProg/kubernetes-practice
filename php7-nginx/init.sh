9f50f75c7779cbc7f303bb4493df05b1d1b6ee6f
annprog <tecbbs@qq.com>
2018-08-27 17:49:59 +0800

    [php7-nginx] fix php conf bug

--- php7-nginx/init.sh
+++ php7-nginx/init.sh
@@ -24,9 +24,9 @@ NGINX_CONF="/etc/nginx/nginx.conf"
 sed -i "s/^pm = .*/pm = $PHP_FPM_MODE/g" $PHP_CONF
 sed -i "s/^pm.max_children = .*/pm.max_children = $PHP_FPM_MAX_CHILDREN/g" $PHP_CONF
 sed -i "s/^pm.start_servers = .*/pm.start_servers = $PHP_FPM_START_SERVERS/g" $PHP_CONF
-sed -i "s/^pm.min_spare_servers = /pm.min_spare_servers = $PHP_FPM_MIN_SPARE_SERVERS/g" $PHP_CONF
-sed -i "s/^pm.max_spare_servers = /pm.max_spare_servers = $PHP_FPM_MAX_SPARE_SERVERS/g" $PHP_CONF
-sed -i "s/^pm.max_requests = /pm.max_requests = $PHP_FPM_MAX_REQUESTS/g" $PHP_CONF
+sed -i "s/^pm.min_spare_servers = .*/pm.min_spare_servers = $PHP_FPM_MIN_SPARE_SERVERS/g" $PHP_CONF
+sed -i "s/^pm.max_spare_servers = .*/pm.max_spare_servers = $PHP_FPM_MAX_SPARE_SERVERS/g" $PHP_CONF
+sed -i "s/^;pm.max_requests = .*/pm.max_requests = $PHP_FPM_MAX_REQUESTS/g" $PHP_CONF
 
 sed -i "s/worker_rlimit_nofile .*/worker_rlimit_nofile $NGINX_WORKER_RLIMIT_NOFILE;/g" $NGINX_CONF
 sed -i "s/worker_connections .*/worker_connections $NGINX_WORKER_CONNECTIONS;/g" $NGINX_CONF
