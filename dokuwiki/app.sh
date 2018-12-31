e8ddcdacabc25eaad3c2065d3f7b4d3f2541232a
annprog <tecbbs@qq.com>
2018-12-31 18:06:07 +0800

    [dokuwiki] default auth and acl

--- dokuwiki/app.sh
+++ dokuwiki/app.sh
@@ -18,3 +18,13 @@ for item in `ls $APP_CONFIG_PATH`; do
 	cp $APP_CONFIG_PATH/$item $WIKIROOT/conf/${item}".php"
 done
 
+# default acl and account
+AUTH="$WIKIROOT/conf/users.auth.php"
+ACL="$WIKIROOT/conf/acl.auth.php"
+if [ ! -f $AUTH ]; then
+	echo 'admin:$1$lgH1ByTw$3agbZNBd7/JrVrUflGVNp/:annProg:admin@admin.com:admin,user' > $AUTH
+fi
+
+if [ ! -f $ACL ]; then
+	echo -e "* @ALL 1\n* @user 8" > $ACL
+fi
