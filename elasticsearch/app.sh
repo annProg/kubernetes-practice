92392e8990517107ec9127fb0ba101ff91569f37
Annhe <tecbbs@qq.com>
2019-02-28 15:29:46 +0800

    [elasticsearch] fix path.data and path.log

--- elasticsearch/app.sh
+++ elasticsearch/app.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-
+[ "$BASEPATH"x == ""x ] && BASEPATH="/data"
 cat > /etc/elasticsearch/elasticsearch.yml <<EOF
 cluster.name: $CLUSTER_NAME
 node.name: $MY_POD_NAME
-path.data: /var/lib/elasticsearch
-path.logs: /var/log/elasticsearch
+path.data: $BASEPATH/lib/elasticsearch
+path.logs: $BASEPATH/log/elasticsearch
 http.host: $MY_POD_IP
 EOF
 
 for item in log lib;do
-	[ ! -d /data/$item/elasticsearch ] && sudo mkdir -p /data/$item/elasticsearch
-	sudo chown -R elasticsearch:elasticsearch /data/$item/elasticsearch
+	[ ! -d $BASEPATH/$item/elasticsearch ] && sudo mkdir -p $BASEPATH/$item/elasticsearch
+	sudo chown -R elasticsearch:elasticsearch $BASEPATH/$item/elasticsearch
 done
 
 sudo rm -f /etc/sudoers.d/elasticsearch
