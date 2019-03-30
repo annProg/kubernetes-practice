4d7aa6fc8fdb7d3cdc7bf9bf41e6a29657f5e8be
Annhe <tecbbs@qq.com>
2019-03-30 18:52:55 +0800

    [elasticsearch] support ES_HEAP_SIZE env

--- elasticsearch/app.sh
+++ elasticsearch/app.sh
@@ -8,6 +8,9 @@ path.logs: $BASEPATH/log/elasticsearch
 http.host: $MY_POD_IP
 EOF
 
+[ "$ES_HEAP_SIZE"x == ""x ] && ES_HEAP_SIZE="1g"
+sudo sed -i -r "s/^-Xm(s|x).*$/-Xm\1$ES_HEAP_SIZE/g" /etc/elasticsearch/jvm.options
+
 for item in log lib;do
 	[ ! -d $BASEPATH/$item/elasticsearch ] && sudo mkdir -p $BASEPATH/$item/elasticsearch
 	sudo chown -R elasticsearch:elasticsearch $BASEPATH/$item/elasticsearch
